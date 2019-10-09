2b7ec5a87e2fd214f30e578f1aece50c79495c21
syedhammadahmed <syedhammadahmed@gmail.com>
2019-10-08 20:58:26 -0400

    FIRST COMMIT
    Signed-off-by: syedhammadahmed <syedhammadahmed@gmail.com>

---
+++ core/gates/QuGateFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by hammad on 10/8/19.
+//
+
+#ifndef UCFQUSIM_QUGATEFACTORY_H
+#define UCFQUSIM_QUGATEFACTORY_H
+
+
+class QuGateFactory {
+
+};
+
+
+#endif //UCFQUSIM_QUGATEFACTORY_H
