3700c0993f199578dcd1d8f4ae3ad6e65ea683c0
syedhammadahmed <syedhammadahmed@gmail.com>
2020-06-30 16:04:04 -0400

    Util file commit

--- util/Util.cpp
+++ util/Util.cpp
@@ -2,6 +2,8 @@
 // Created by YUSHAH on 10/21/2019.
 //
 
+#include <time.h>
+#include <ctime>
 #include "Util.h"
 
 string Util::FILE_EXTENSION = ".qasm";
@@ -24,3 +26,20 @@ double Util::timeIt(bool isEnd) {
     }
     return 0.0;
 }
+
+
+
+// ubuntu
+//double Util::timeIt(bool isEnd) {
+//    static time_t start;
+//    static time_t end;
+//    if(!isEnd) {
+//        time(&start);
+//    }
+//    else {
+//        time(&end);
+//        return double(end) - double(start);
+//    }
+//    return 0.0;
+//}
+
