8e8649f32b98794d06b2bb58396e417859ef288a
syedhammadahmed <syedhammadahmed@gmail.com>
2021-03-09 15:04:30 -0500

    elementary code generation complete

--- util/Util.cpp
+++ util/Util.cpp
@@ -248,7 +248,7 @@ void Util::randomSampling(vector<QuMapping>& population, vector<QuMapping>& samp
 
 vector<int> Util::makeMappingVector() {
     vector<int> mappingVector(Constants::QX5_N);
-    const int *mappingArray = Constants::HARDCODED_INIT_MAPPINGS[Constants::rd73_140];
+    const int *mappingArray = Constants::HARDCODED_INIT_MAPPINGS[Constants::sqn_258];
     memcpy(&mappingVector[0], mappingArray, Constants::QX5_N*sizeof(int));
     return mappingVector;
 }
