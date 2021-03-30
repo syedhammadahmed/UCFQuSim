f9873f77edff47f83121ebd3e098c3a2611f4a9e
syedhammadahmed <syedhammadahmed@gmail.com>
2021-03-30 17:36:00 -0400

    elementary code generation complete

--- evaluator/QuMultiEvaluator.cpp
+++ evaluator/QuMultiEvaluator.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <direct.h>
+//#include <direct.h>
 //#include <direct.h>
 #include <cstdio>
 
@@ -55,8 +55,8 @@ void QuMultiEvaluator::deletePreviousFiles() {
 
     dir = opendir(directory.c_str());
     if(dir == NULL)
-        _mkdir(directory.c_str());
-//        mkdir(directory.c_str(), 0777);
+//        _mkdir(directory.c_str());
+        mkdir(directory.c_str(), 0777);
 
     while ((dirPtr = readdir(dir)) != NULL) {
         if(string(dirPtr->d_name) == "." || string(dirPtr->d_name) == "..")
